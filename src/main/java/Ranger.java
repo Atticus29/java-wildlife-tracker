45771b0dc3e71d77d78ffe90b1c2d71c05ec6478
Mark <mark.aaron.fisher@gmail.com>
2017-04-07 19:36:26 -0700

    fix typo in save method, allowing all tests for ranger class to pass

---
+++ src/main/java/Ranger.java
@@ -0,0 +1,145 @@
+import org.sql2o.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.sql.Timestamp;
+import java.util.regex.*;
+
+public class Ranger implements DatabaseManagement {
+  private int badge_number;
+  private String name;
+  public static final int MAX_NAME_LENGTH = 30;
+  private String email;
+  private int id;
+
+
+  public Ranger(String name, int badgeNumber, String email) {
+    if(name.length() > MAX_NAME_LENGTH){
+      throw new UnsupportedOperationException("Name is too long for the database.");
+    } else{
+      // make it throw an error if someone already has that name
+      this.name = name;
+    }
+
+    // if(!(badgeNumber instanceof int)){
+    //   throw new UnsupportedOperationException ("Badge number is not a number");
+    // } else{
+      this.badge_number = badgeNumber;
+    // }
+
+    String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    if(!Pattern.matches(emailPattern, email)){
+      throw new UnsupportedOperationException ("Not a valid email address");
+    } else{
+      this.email = email;
+    }
+  }
+
+  public int getId(){
+    return this.id;
+  }
+
+  public int getBadge_number(){
+    return this.badge_number;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public String getEmail(){
+    return this.email;
+  }
+
+  @Override
+  public boolean equals(Object otherRanger) {
+    if(!(otherRanger instanceof Ranger)) {
+      return false;
+    } else {
+      Ranger newRanger = (Ranger) otherRanger;
+      return this.getName().equals(newRanger.getName()) &&
+      this.getBadge_number() == newRanger.getBadge_number() &&
+      this.getId() == newRanger.getId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      // make it throw an error if someone already has that name?
+      String sql = "INSERT INTO Rangers (badge_number, name, email) VALUES (:badge_number, :name, :email);";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("badge_number", this.badge_number)
+        .addParameter("name", this.name)
+        .addParameter("email", this.email)
+        .throwOnMappingFailure(false)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Ranger> allRangers() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM rangers;";
+      return con.createQuery(sql)
+        // .throwOnMappingFailure(false)
+        .executeAndFetch(Ranger.class);
+    }
+  }
+
+  public static Ranger findRanger(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM rangers WHERE id=:id;";
+      Ranger Ranger = con.createQuery(sql)
+        .throwOnMappingFailure(false)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Ranger.class);
+      return Ranger;
+    }
+  }
+
+  public void updateName(String name) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE Rangers SET name=:name WHERE id=:id;";
+      con.createQuery(sql)
+        .addParameter("id", this.id)
+        .addParameter("name", name)
+        .executeUpdate();
+    }
+  }
+
+  public void updateBadgeNumber(int badgeNum) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE Rangers SET badge_number=:badge_number WHERE id=:id;";
+      con.createQuery(sql)
+        .addParameter("id", this.id)
+        .addParameter("badge_number", badgeNum)
+        .executeUpdate();
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM rangers WHERE id=:id;";
+      con.createQuery(sql)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  public List<Sighting> getSightings() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM sightings WHERE Ranger_id=:id;";
+        List<Sighting> sightings = con.createQuery(sql)
+          .addParameter("id", this.id)
+          .throwOnMappingFailure(false)
+          .executeAndFetch(Sighting.class);
+      return sightings;
+    }
+  }
+
+
+}
---
+++ src/test/java/RangerTest.java
@@ -0,0 +1,96 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.sql.Timestamp;
+
+public class RangerTest {
+  private Ranger testRanger;
+
+  @Before
+  public void setUp(){
+    testRanger = new Ranger("Bill", 321, "mark.aaron.fisher@gmail.com");
+    testRanger.save();
+  }
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void ranger_instantiatesCorrectly_true(){
+    assertTrue(testRanger instanceof Ranger);
+  }
+
+  @Test (expected = UnsupportedOperationException.class)
+  public void ranger_throwsExceptionIfNameTooLong(){
+    Ranger longRanger = new Ranger("antidisestablishmentarianismusson", 321, "mark.aaron.fisher@gmail.com");
+  }
+
+  // @Test (expected = UnsupportedOperationException.class)
+  // public void ranger_throwsExceptionIfBadgeNotANumber(){
+  //   Ranger longRanger = new Ranger("Bill", "321a", "mark.aaron.fisher@gmail.com");
+  // }
+
+  @Test (expected = UnsupportedOperationException.class)
+  public void ranger_throwsExceptionIfEmailNotValid(){
+    Ranger longRanger = new Ranger("Bill", 321,  "@mark.aaron.fisher.gmail.com");
+  }
+
+  @Test
+  public void save_assignsIdToObjectAndSavesObjectToDatabase() {
+    Ranger savedRanger = Ranger.allRangers().get(0);
+    assertEquals(testRanger.getId(), savedRanger.getId());
+  }
+
+  @Test
+  public void updateBadgeNumber_changesBadgeNumber_true(){
+    testRanger.updateBadgeNumber(100);
+    assertEquals(100,Ranger.findRanger(testRanger.getId()).getBadge_number());
+  }
+
+  @Test
+  public void updateName_changesName_true(){
+    testRanger.updateName("Janet");
+    assertEquals("Janet", Ranger.findRanger(testRanger.getId()).getName());
+  }
+
+  @Test
+  public void getters_getWhatTheyNeedToGet_true(){
+    assertEquals("Bill", testRanger.getName());
+    assertEquals(321, testRanger.getBadge_number());
+    assertEquals("mark.aaron.fisher@gmail.com", testRanger.getEmail());
+    assertTrue(testRanger.getId()>0);
+  }
+
+  @Test
+  public void equals_returnsTrueIfNameIsTheSame_true() {
+    Ranger retrievedRanger = Ranger.findRanger(testRanger.getId());
+    assertTrue(testRanger.equals(retrievedRanger));
+  }
+
+  @Test
+  public void delete_deletesRangerFromDatabase_0() {
+    testRanger.delete();
+    assertEquals(0, Ranger.allRangers().size());
+  }
+
+  @Test
+  public void allRangers_returnsAllInstancesOfRanger_true() {
+    Ranger secondTestRanger = new Ranger("Stacey", 411, "fishema@ohsu.edu");
+    secondTestRanger.save();
+    assertEquals(true, Ranger.allRangers().get(0).equals(testRanger));
+    assertEquals(true, Ranger.allRangers().get(1).equals(secondTestRanger));
+  }
+
+  @Test
+  public void findRanger_returnsNullWhenNoRangerFound_null() {
+    assertTrue(Ranger.findRanger(999) == null);
+  }
+
+
+}
